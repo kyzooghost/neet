78b22f9900723122a03800464e49638ba269e4bc
kyzooghost <jeffersonzjtang@gmail.com>
2024-05-07 16:49:42 +1000

    added set solution for contains-duplicate

--- problems/contains-duplicate.py
+++ problems/contains-duplicate.py
@@ -1,6 +1,10 @@
 # Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
+# Dict/hashset - O(N) space complexity, tradeoff space efficiency for time efficiency
+# Otherwise sort the array and check if adjacent elements are equal - O(1) space efficiency, O(N log N) time efficiency for sort
+
 class Solution(object):
+    # Dict solution
     def containsDuplicate(self, nums):
         """
         :type nums: List[int]
@@ -13,4 +17,17 @@ class Solution(object):
                 return True
             dict[num] = True
         return False
-        
+    
+    # Set solution
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+
+        set = set()
+        for num in nums:
+            if num in set:
+                return True
+            set.add(num)
+        return False
