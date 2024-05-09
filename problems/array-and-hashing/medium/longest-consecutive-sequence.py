62af3fb38c171053266ba043a2bc952487438f07
kyzooghost <jeffersonzjtang@gmail.com>
2024-05-10 09:30:14 +1000

    did neetcode solution for longest-consecutive-sequence

--- problems/array-and-hashing/medium/longest-consecutive-sequence.py
+++ problems/array-and-hashing/medium/longest-consecutive-sequence.py
@@ -3,7 +3,48 @@
 
 class Solution(object):
 
-    # There should be a solution where 
+    # 92% runtime, 52% memory
+    # O(M) memory for set, where M < N and M = # of distinct numbers in N
+    # O(N) runtime
+    #
+    # Big improvements
+    # - No need to store sequences, nor maintain a hashmap mapping num to sequence
+    # - set API - set(<Iterable>), in set, not in set, .add
+    # - Only care about storing the max sequence length found
+    # - Guaranteed two passes max through each number
+    # - None of this 'sequence join' bs. We only need the max sequence length at the end, nothing else
+    # - Single pass through nums -> find all sequence starts -> Can run through all sequences
+
+    # Correct big ideas
+    # - Do check if num to left or right
+    def longestConsecutive_Neet(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) == 0:
+            return 0
+        
+        num_set = set(nums)
+
+        resp = 1
+        for num in num_set:
+            sequence_length = 1
+            # Check if num is sequence start
+            if num - 1 not in num_set:
+                # Loop through sequence
+                i = 1
+                while num + i in num_set:
+                    sequence_length += 1
+                    i += 1
+                if sequence_length > resp:
+                    resp = sequence_length
+            # Not sequence start, so go to next number
+            else:
+                continue
+        
+        return resp
+            
 
     # 44% runtime, 5% memory
     def longestConsecutive(self, nums):
@@ -61,7 +102,7 @@ class Solution(object):
         return resp
 
 solution = Solution()
-print(solution.longestConsecutive([100,4,200,1,3,2]))
+print(solution.longestConsecutive_Neet([100,4,200,1,3,2]))
 print(solution.longestConsecutive([0,3,7,2,5,8,4,6,0,1]))
 print(solution.longestConsecutive([]))
 print(solution.longestConsecutive([1,2,0,1]))
