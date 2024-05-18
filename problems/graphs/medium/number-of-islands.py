3a28306ba8cbb1f6d32ab4e3101ada23b703f759
kyzooghost <jeffersonzjtang@gmail.com>
2024-05-18 23:08:53 +1000

    did number of islands

--- problems/graphs/medium/number-of-islands.py
+++ problems/graphs/medium/number-of-islands.py
@@ -2,6 +2,50 @@
 
 # An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
 
+# 10% runtime, 15% memory
+# BFS is a bit meh, rather do recursive DFS with base cases at the start of the function
+class Solution_Neet(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        if not grid:
+            return 0
+        
+        height, width = len(grid), len(grid[0])
+        visited = set()
+        islands = 0
+
+        def bfs(y, x):
+            q = collections.deque()
+            visited.add((y, x))
+            q.append((y, x))
+            while q:
+                cur_y, cur_x = q.popleft()
+                directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+                for y_diff, x_diff in directions:
+                    new_y, new_x = cur_y + y_diff, cur_x + x_diff
+                    if new_y not in range(height):
+                        continue
+                    if new_x not in range(width):
+                        continue
+                    if grid[new_y][new_x] == "0":
+                        continue
+                    if (new_y, new_x) in visited:
+                        continue
+                    q.append((new_y, new_x))
+                    visited.add((new_y, new_x))
+
+
+        for y in range(height):
+            for x in range(width):
+                if grid[y][x] == "1" and (y, x) not in visited:
+                    bfs(y, x)
+                    islands += 1
+        
+        return islands
+
 # 38% runtime, 5% memory
 # Easier version of word-search, sigh had the correct idea and implementation except thought we were dealing with int type and not str type
 class Solution(object):
