8d1069ac16b7cd559799363231e2d4cbe6a6a588
kyzooghost <jeffersonzjtang@gmail.com>
2024-05-23 20:33:06 +1000

    did coin change

--- problems/dynamic-programming/medium/coin-change.py
+++ problems/dynamic-programming/medium/coin-change.py
@@ -7,6 +7,88 @@ from collections import deque
 # How can we reduce this to a decision tree?
 # Fewest number = largest coin #s
 
+# 60% runtime, 73% memory
+# Yep using array vs dict seems to perform much better, even if array contains some sentinel values where dict does not have values
+class Solution_AfterNeetIterativeDP_V2:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        if amount == 0: return 0
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0
+
+        for i in range(1, amount + 1):
+            # Iterate through all coins
+            for coin in coins:
+                remainder = i - coin
+                if remainder >= 0:
+                    dp[i] = min(dp[i], 1 + dp[remainder])
+
+        return dp[amount] if dp[amount] != amount + 1 else -1
+
+class Solution_Neet:
+    # Wait why does this get 73% runtime, 73% memory even though time + space complexity is the same as what I implemented? Is it because of using an array?
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0
+        for a in range(1, amount + 1):
+            for c in coins:
+                if a - c >= 0:
+                    dp[a] = min(dp[a], 1 + dp[a - c])
+        return dp[amount] if dp[amount] != amount + 1 else -1
+
+
+# Ok this works, but idk DP is still easier to reason about and implement
+# Got TLE with top-bottom recursive on Leetcode anyway lol
+class Solution_AfterNeetRecursiveMemoization:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        if amount == 0: return 0
+        self.dict = {}
+
+        def dfs(remainder, depth):
+            if remainder < 0:
+                return None
+            if remainder == 0:
+                return depth
+            if remainder in self.dict:
+                return depth + self.dict[remainder]
+
+            min_depth = float("inf")
+            for coin in coins:
+                dfs_depth = dfs(remainder - coin, depth + 1)
+                if dfs_depth: min_depth = min(min_depth, dfs_depth)
+
+            if min_depth == float("inf"): 
+                return None
+            else:
+                self.dict[remainder] = min_depth - depth
+                return min_depth
+
+        dfs(amount, 0)
+        return self.dict.get(amount, -1)
+
+# Ok watched Neet video, still feel meh about this solution - O(amount) Space and O(len(coins) * amount) time
+# Urgh finally accepted, it just feels really weird because it's still an O(len(coins) * amount) time efficiency solution
+# But yea greedy doesn't work, and backtracking is O(N^N), so what are you left with?
+# You could probably do this in O(1) space, but the bookkeeping would become much more complex
+# 5% runtime, 21% memory
+class Solution_AfterNeetIterativeDP:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        if amount == 0: return 0
+        dict = { 0 : 0 }
+
+        for i in range(1, amount + 1):
+            # Iterate through all coins
+            for coin in coins:
+                remainder = i - coin
+                if remainder < 0: continue
+                elif remainder == 0: dict[coin] = 1
+                else:
+                    if remainder in dict:
+                        dict[i] = min(dict[remainder] + 1, dict.get(i, float("inf")))
+                    else:
+                        continue
+
+        return dict.get(amount, -1)
+
 # Ok urghhhhh, tried for 1hr 50mins, couldn't get a working solution
 class Solution_V3:
     def coinChange(self, coins: List[int], amount: int) -> int:
@@ -140,11 +222,10 @@ class Solution:
 
         return self.resp
 
-sln = Solution_V3()
-# print(sln.coinChange([1,5,10], 12)) #3
-# print(sln.coinChange([2], 3)) #-1
-# print(sln.coinChange([1], 0)) #0
-# print(sln.coinChange([1], 1)) #1
+sln = Solution_AfterNeetRecursiveMemoization()
+print(sln.coinChange([1,5,10], 12)) #3
+print(sln.coinChange([2], 3)) #-1
+print(sln.coinChange([1], 0)) #0
+print(sln.coinChange([1], 1)) #1
 print(sln.coinChange([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 100)) #10
-
 
