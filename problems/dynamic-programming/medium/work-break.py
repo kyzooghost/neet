ba876e322d41110aa81c05fb9d2401c089bfbbba
kyzooghost <jeffersonzjtang@gmail.com>
2024-05-24 22:12:47 +1000

    did word-break

--- problems/dynamic-programming/medium/work-break.py
+++ problems/dynamic-programming/medium/work-break.py
@@ -1,5 +1,39 @@
 from typing import List
 
+# Urgh ok watched Neet, using a trie to store the dict is the completely wrong direction
+# Because the DP twist here is that dp[n] does not depend on dp[n-1], but dp[n - len(word)]. Yes you are jumping indexes. And because you are jumping indexes and not just using the last, you need O(N) storage and not O(1)
+# Yea urgh, still doesn't feel intuitive to me because it's still a crazy brute force O(MNP) time complexity. Is it just because there is no more efficient way to solve this? Using a trie to do a single O(N) traversal just plain doesn't work. Then backtracking with a trie has some ridiculous time complexity and doesn't lend to memoization. Because if you go one character a time, it's a bit difficult to break it down into distinct repeating subtrees.
+# But sigh, this is still 60% runtime and 56% memory...
+class Solution_AfterWatchNeet:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp = [False] * (len(s) + 1)
+        dp[len(s)] = True
+
+        # O(N) where N = len(s)
+        for i in range(len(s) - 1, -1, -1):
+            # O(M) where M = len(wordDict)
+            for word in wordDict:
+                # O(P) where p = max word length
+                
+                # Out of bounds
+                if i + len(word) > len(s):
+                    continue
+                # Substring match -> use dp array from previously
+                if s[i:i+len(word)] == word:
+                    if dp[i + len(word)]:
+                        dp[i] = True
+                        break
+        return dp[0]
+
+sln = Solution_AfterWatchNeet()
+print(sln.wordBreak('leetcode', ["leet", "code"])) #T
+print(sln.wordBreak('neetcode', ["neet", "code"])) #T
+print(sln.wordBreak('applepenapple', ["apple", "pen"])) #T
+print(sln.wordBreak('catsandog', ["cats","dog","sand","and","cat"])) #F
+print(sln.wordBreak('catsincars', ["cats","cat","sin","in","car"])) #F
+print(sln.wordBreak('aaaaaaa', ["aaaa","aaa"])) #T
+print(sln.wordBreak('bb', ["a","b","bbb","bbbb"])) #T
+
 class Trie(object):
     def __init__(self, isRoot = False):
         self.children = [None] * 26
@@ -47,16 +81,6 @@ class Solution_V3:
 
         dfs(0, self.trie)
         return self.resp
-
-sln = Solution_V3()
-print(sln.wordBreak('leetcode', ["leet", "code"])) #T
-print(sln.wordBreak('neetcode', ["neet", "code"])) #T
-print(sln.wordBreak('applepenapple', ["apple", "pen"])) #T
-print(sln.wordBreak('catsandog', ["cats","dog","sand","and","cat"])) #F
-print(sln.wordBreak('catsincars', ["cats","cat","sin","in","car"])) #F
-print(sln.wordBreak('aaaaaaa', ["aaaa","aaa"])) #T
-print(sln.wordBreak('bb', ["a","b","bbb","bbbb"])) #T
-
 
 # Lol, this is just greedy on the opposite side - pass 35/46 cases
 # So either way, greedy is failing here - "bb" and ["a","b","bbb","bbbb"]
