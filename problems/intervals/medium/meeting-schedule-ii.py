03929be83ca1e87b70c9945d88913eaf94aeea9a
kyzooghost <jeffersonzjtang@gmail.com>
2024-05-29 11:07:45 +1000

    did another solution for meeting-schedule-ii

--- problems/intervals/medium/meeting-schedule-ii.py
+++ problems/intervals/medium/meeting-schedule-ii.py
@@ -4,6 +4,30 @@ class Interval(object):
     def __init__(self, start, end):
         self.start = start
         self.end = end
+        
+# Cool, O(N lg N) method to solve the problem
+class Solution_AfterNeet:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        if len(intervals) == 0: return 0
+        start, end = [], []
+        for i in range(len(intervals)):
+            start.append(intervals[i].start)
+            end.append(intervals[i].end)
+
+        start.sort()
+        end.sort()
+        s, e, resp, meetings = 0, 0, 0, 0
+        while s < len(start) and e < len(start):
+            # New meeting start
+            if start[s] < end[e]:
+                s += 1
+                meetings += 1
+            # Meeting end
+            elif end[e] <= start[s]:
+                e += 1
+                meetings -= 1
+            resp = max(resp, meetings)
+        return resp
 
 # Thinking can we reuse the solution for non-overlapping-intervals
 # Ok this does work, but worst case O(N^2)
