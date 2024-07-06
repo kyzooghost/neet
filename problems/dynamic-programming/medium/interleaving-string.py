f7483323be237fb1c7f6013a2de84e49eabcdaa9
kyzooghost <73516204+kyzooghost@users.noreply.github.com>
2024-07-06 19:10:26 +1000

    iterative solution for interleaving-string

--- problems/dynamic-programming/medium/interleaving-string.py
+++ problems/dynamic-programming/medium/interleaving-string.py
@@ -1,3 +1,24 @@
+# Hmm...still tough to think about it backwards imo
+# More straightforward to run through a decision tree using DFS, and cache the positions
+class Solution_V3(object):
+    def isInterleave(self, s1, s2, s3):
+        """
+        :type s1: str
+        :type s2: str
+        :type s3: str
+        :rtype: bool
+        """
+        if len(s1) + len(s2) != len(s3): return False
+        dp = [[False for _ in range(len(s2) + 1)] for _ in range(len(s1) + 1)]
+        dp[len(s1)][len(s2)] = True
+
+        for i in range(len(s1), -1, -1):
+            for j in range(len(s2), -1, -1):
+                down, right = False, False
+                if i < len(s1) and s1[i] == s3[i + j] and dp[i+1][j]: down = True
+                if j < len(s2) and s2[j] == s3[i + j] and dp[i][j+1]: right = True
+                dp[i][j] = dp[i][j] or down or right
+        return dp[0][0]
 
 class Solution_V2(object):
     def isInterleave(self, s1, s2, s3):
@@ -8,6 +29,8 @@ class Solution_V2(object):
         :rtype: bool
         """
         if len(s1) + len(s2) != len(s3): return False
+        # O(MN) space, O(MN) time
+        # Brute force is O(2^(M + N))
         dp = {}
         def dfs(i, j):
             if i == len(s1) and j == len(s2): return True
